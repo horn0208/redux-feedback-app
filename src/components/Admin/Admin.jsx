074d33ec3468656793ef50905a4f5952ca447646
Liz Horn <liz.mpls@gmail.com>
2022-06-18 18:17:42 -0500

    result table displays on DOM. need to remove id

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+// mui table imports
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function Admin( props ){
 
@@ -25,7 +33,37 @@ function Admin( props ){
     return(
         <div>
             <h2>Feedback Results</h2>
-            
+            <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell align="right">Feeling</TableCell>
+                        <TableCell align="right">Comprehension</TableCell>
+                        <TableCell align="right">Support</TableCell>
+                        <TableCell align="right">Comments</TableCell>
+                        <TableCell align="right">Delete</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {answers.map((row) => (
+                        <TableRow
+                        key={row.id}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                        <TableCell component="th" scope="row">
+                            {row.id}
+                        </TableCell>
+                        <TableCell align="right">{row.feeling}</TableCell>
+                        <TableCell align="right">{row.understanding}</TableCell>
+                        <TableCell align="right">{row.support}</TableCell>
+                        <TableCell align="right">{row.comments}</TableCell>
+                        <TableCell align="right">delete btn</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
         </div>
     );
 }
