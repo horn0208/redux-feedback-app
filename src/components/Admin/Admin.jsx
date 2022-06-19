fbdf4d3ab3c090890750ba0ce8b29bd4c5632161
Liz Horn <liz.mpls@gmail.com>
2022-06-19 10:44:57 -0500

    confirm delete modal

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -10,10 +10,21 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import IconButton from '@mui/material/IconButton';
 import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
+// mui 'confirm delete' modal imports
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
 
 function Admin( props ){
-
-    const [answers, setAnswers] = useState([]);
+    // hook for array of responses
+    let [answers, setAnswers] = useState([]);
+    // hook for modal display
+    const [open, setOpen] = useState(false);
+    // hook for id of item to delete
+    const [idToDelete, setIdToDelete] = useState(0);
     
     //make GET call on page load
     useEffect(()=>{
@@ -32,9 +43,22 @@ function Admin( props ){
         })
     }
 
-    const deleteItem=(itemID)=>{
-        console.log('in deleteClick:', itemID)
+    // delete item after delete btn is clicked in modal
+    const deleteItem=()=>{
+        console.log('in deleteItem:', idToDelete);
+        // DELETE request
+
+        handleClose();
     }
+
+    // click handlers to open/close modal
+    const handleClickOpen = (itemID) => {
+        setOpen(true);
+        setIdToDelete(itemID)
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     return(
         <div>
@@ -55,18 +79,35 @@ function Admin( props ){
                         <TableRow
                             key={row.id}
                             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                        <TableCell component="th" scope="row">{row.feeling}</TableCell>
-                        <TableCell align="center">{row.understanding}</TableCell>
-                        <TableCell align="center">{row.support}</TableCell>
-                        <TableCell align="center">{row.comments}</TableCell>
-                        <TableCell align="right">
-                            <IconButton
-                                aria-label='delete'
-                                color='primary'
-                                onClick={()=>deleteItem(row.id)}>
-                                <DeleteSweepIcon/>
-                            </IconButton>
-                        </TableCell>
+                            <TableCell component="th" scope="row">{row.feeling}</TableCell>
+                            <TableCell align="center">{row.understanding}</TableCell>
+                            <TableCell align="center">{row.support}</TableCell>
+                            <TableCell align="center">{row.comments}</TableCell>
+                            <TableCell align="right">
+                                <IconButton
+                                    aria-label='delete'
+                                    color='primary'
+                                    onClick={()=>{handleClickOpen(row.id)}}>
+                                    <DeleteSweepIcon/>
+                                </IconButton>
+                                <Dialog
+                                    open={open}
+                                    onClose={handleClose}
+                                    aria-labelledby="alert-dialog-title"
+                                    aria-describedby="alert-dialog-description">
+                                    <DialogContent>
+                                    <DialogContentText id="alert-dialog-description">
+                                        Are you sure you want to permanently delete this feedback response?
+                                    </DialogContentText>
+                                    </DialogContent>
+                                    <DialogActions>
+                                    <Button onClick={handleClose}>Cancel</Button>
+                                    <Button onClick={deleteItem} autoFocus>
+                                        Delete
+                                    </Button>
+                                    </DialogActions>
+                                </Dialog>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
